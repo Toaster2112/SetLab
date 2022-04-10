b38ce6cf01657830480c05891005d8a18e1cdc86
Toaster2112 <103362160+Toaster2112@users.noreply.github.com>
2022-04-10 06:45:20 -0400

    Update SetSecondary1L.java

--- SetSecondary1L.java
+++ SetSecondary1L.java
@@ -1,1 +1,39 @@
+import components.set.Set;
+import components.set.Set1L;
 
+/**
+ * Layered implementations of secondary methods {@code add} and {@code remove}
+ * for {@code Set}.
+ * 
+ * @param <T>
+ *            type of {@code Set} elements
+ */
+public final class SetSecondary1L<T> extends Set1L<T> {
+
+    /**
+     * No-argument constructor.
+     */
+    public SetSecondary1L() {
+        super();
+    }
+
+    @Override
+    public Set<T> remove(Set<T> s) {
+        assert s != null : "Violation of: s is not null";
+        assert s != this : "Violation of: s is not this";
+
+        Set<T> result = this.remove(s);
+      return result
+
+    }
+
+    @Override
+    public void add(Set<T> s) {
+        assert s != null : "Violation of: s is not null";
+        assert s != this : "Violation of: s is not this";
+
+        this.add(s);
+
+    }
+
+}
